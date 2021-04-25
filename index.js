4e5ebea9a7d1399c117d9bafb54f47ce6bb967e1
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-24 21:49:17 -0400

    commit tree 8cf65193097bbd57f6d49dfade757aca643b45a2

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Add your code here
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// }); 
+
+
+
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   let configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+// fetch("http://localhost:3000/dogs", configObj)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(object) {
+//     console.log(object);
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   }); 
+ 
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+function submitData() {
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object["id"]
+        })
+    .catch(function(error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message
+        }); 
+}
+
+submitData()
